0c832cd602b567e3d01f9130b93ef4e6dcc88d2b
Eddie Lee <eddieberklee@gmail.com>
2016-04-28 01:46:51 -0700

    A decent start on handling the dynamics of the card drag

--- app/src/main/java/com/compscieddy/feels/CardOnTouchListener.java
+++ app/src/main/java/com/compscieddy/feels/CardOnTouchListener.java
@@ -3,18 +3,36 @@ package com.compscieddy.feels;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.compscieddy.eddie_utils.Etils;
+import com.compscieddy.eddie_utils.Lawg;
+import com.facebook.rebound.SimpleSpringListener;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringSystem;
 
 import java.util.List;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by elee on 4/27/16.
  */
 public class CardOnTouchListener implements View.OnTouchListener {
 
+  private static final Lawg lawg = Lawg.newInstance(CardOnTouchListener.class.getSimpleName());
+
   boolean isDragging;
   List<ViewGroup> mCards;
   int currentIndex = 0;
-
+  float viewStartX, viewStartY;
+  float startX, startY;
+  float currentX, currentY;
+  float differenceY;
+  float maxY;
+  final float THRESHOLD = Etils.dpToPx(200);
+  private ViewGroup mCurrentCard;
+  private TextView debugText;
 
   public CardOnTouchListener(List<ViewGroup> cards) {
     mCards = cards;
@@ -22,33 +40,97 @@ public class CardOnTouchListener implements View.OnTouchListener {
 
   @Override
   public boolean onTouch(View v, MotionEvent event) {
-    switch (event.getAction()) {
 
+    currentX = event.getX();
+    currentY = event.getY();
+    maxY = v.getHeight();
+    differenceY = currentY - startY;
+
+    debugText = ButterKnife.findById(v, R.id.debug_text);
+    mCurrentCard = mCards.get(currentIndex);
+
+    switch (event.getAction()) {
 
       case MotionEvent.ACTION_DOWN:
         isDragging = true;
+        startX = event.getX();
+        startY = event.getY();
         return true;
 
 
       case MotionEvent.ACTION_MOVE:
-        updateCardLocation(event);
+        debugText.setText("(" + startX + ", " + startY + ", " + differenceY + ")");
+        updateCardLocation();
         return true;
 
 
       case MotionEvent.ACTION_UP:
         isDragging = false;
+        resetCardLocation();
         return true;
-
 
     }
     return false;
   }
 
-  public void updateCardLocation(MotionEvent event) {
-    float x = event.getRawX();
-    float y = event.getRawY();
-    ViewGroup currentCard = mCards.get(currentIndex);
-    currentCard.setY(y);
+  public void updateCardLocation() {
+
+    lawg.d("Y:" + mCurrentCard.getY() + " Y being translated: " + differenceY);
+    float centerY = maxY / 2;
+    // TODO: this is highly dependent on the state of the current card position (or wait,
+    // no, I just need to update the current index I guess
+    mCurrentCard.setY(centerY + differenceY);
+
+  }
+
+  public void resetCardLocation() {
+
+    SpringSystem springSystem = SpringSystem.create();
+    Spring springReset = springSystem.createSpring();
+    Spring springUp = springSystem.createSpring();
+    Spring springDown = springSystem.createSpring();
+
+    springReset.addListener(new SimpleSpringListener() {
+      @Override
+      public void onSpringUpdate(Spring spring) {
+        float value = (float) spring.getCurrentValue();
+        float centerY = maxY / 2;
+        float distanceFromReset = currentY - centerY;
+        float translationY = (1 - value) * distanceFromReset;
+        mCurrentCard.setY(centerY + translationY);
+      }
+    });
+
+    springUp.addListener(new SimpleSpringListener() {
+      @Override
+      public void onSpringUpdate(Spring spring) {
+        float value = (float) spring.getCurrentValue();
+        float distanceFromTop = currentY;
+        float translationY = (1 - value) * distanceFromTop;
+        mCurrentCard.setY(translationY);
+      }
+    });
+
+    springDown.addListener(new SimpleSpringListener() {
+      @Override
+      public void onSpringUpdate(Spring spring) {
+        float value = (float) spring.getCurrentValue();
+        float distanceBottom = maxY - currentY;
+        float translationY = (1 - value) * distanceBottom;
+        mCurrentCard.setTranslationY(translationY);
+      }
+    });
+
+    if (differenceY < -THRESHOLD) {
+      lawg.d("SPRING_UP ");
+      springUp.setEndValue(1.0f);
+    } else if (differenceY > THRESHOLD) {
+      lawg.d("SPRING_DOWN ");
+      springDown.setEndValue(1.0f);
+    } else {
+      lawg.d("SPRING_RESET ");
+      springReset.setEndValue(1.0f);
+    }
 
   }
 
--- app/src/main/java/com/compscieddy/feels/MainActivity.java
+++ app/src/main/java/com/compscieddy/feels/MainActivity.java
@@ -4,25 +4,47 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+
+import com.compscieddy.eddie_utils.Lawg;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewTreeObserver.OnGlobalLayoutListener {
+
+  private static final Lawg lawg = Lawg.newInstance(MainActivity.class.getSimpleName());
 
   @Bind(R.id.card_work) ViewGroup mCardWork;
   @Bind(R.id.card_fitness) ViewGroup mCardFitness;
+  private View mRootView;
+  ArrayList<ViewGroup> mCards = new ArrayList<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    View rootView = getLayoutInflater().inflate(R.layout.activity_main, null);
-    setContentView(rootView);
+    mRootView = getLayoutInflater().inflate(R.layout.activity_main, null);
+    setContentView(mRootView);
+    ButterKnife.bind(this);
 
-    ArrayList<ViewGroup> mCards = new ArrayList<>();
     mCards.add(mCardWork);
     mCards.add(mCardFitness);
-    rootView.setOnTouchListener(new CardOnTouchListener(mCards));
+    mRootView.setOnTouchListener(new CardOnTouchListener(mCards));
+
+    mRootView.getViewTreeObserver().addOnGlobalLayoutListener(MainActivity.this);
   }
+
+  @Override
+  public void onGlobalLayout() {
+    initCards();
+    mRootView.getViewTreeObserver().removeOnGlobalLayoutListener(MainActivity.this);
+  }
+
+  public void initCards() {
+    lawg.d("Set Y: " + mRootView.getHeight() / 2);
+    mCardWork.setY(mRootView.getHeight() / 2);
+  }
+
 }
